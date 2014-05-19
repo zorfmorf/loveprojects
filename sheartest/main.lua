78b81f5e06cdc4fc6cd9014a8cbf524aee5ba21a
Sebastian Tauch <sebastian.tauch@gmx.de>
2014-05-19 15:10:16 +0200

    Finally got shearing conversion working

--- sheartest/main.lua
+++ sheartest/main.lua
@@ -1,31 +1,14 @@
-x = 700
-y = 700
-width = 50
-height = 50
-
 circle = { x=0, y=0}
-angle = -0.3
+k = math.random() * math.pi
 
 function love.draw()
-
-    --love.graphics.rectangle('line', x, y, width, height)
-    
-    love.graphics.shear(angle, angle)
-    love.graphics.rectangle('line', x, y, width, height)
-    --[[
-    love.graphics.origin()
-    love.graphics.shear(0, angle)
-    love.graphics.shear(angle, 0)
-    love.graphics.setColor(255, 200, 200)
-    love.graphics.rectangle('line', x, y, width, height)
-    love.graphics.setColor(255, 255, 255) ]]--
-    
-    love.graphics.circle("fill", circle.x, circle.y, 5, 20)
+    love.graphics.shear(k, k)
+    love.graphics.circle("fill", circle.x, circle.y, 2, 20)
 end
 
-function love.mousepressed(x, y, button)
+function love.mousepressed(nx, ny, button)
     if button == "l" then
-        circle.y = y - math.sin(angle) * x
-        circle.x = x - math.sin(angle) * y
+        circle.y = (ny - nx * k) / (1 - k * k)
+        circle.x = (nx - ny * k) / (1 - k * k)
     end
 end
